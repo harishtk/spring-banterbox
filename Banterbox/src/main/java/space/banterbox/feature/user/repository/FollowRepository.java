8c84d8123783bc3885bf71eeb562646183b88858
Haris <harishtk95@gmail.com>
2025-07-06 22:30:08 +0530

    Fix env and code refactor
    Signed-off-by: Haris <harishtk95@gmail.com>

--- Banterbox/src/main/java/space/banterbox/feature/user/repository/FollowRepository.java
+++ Banterbox/src/main/java/space/banterbox/feature/user/repository/FollowRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import space.banterbox.feature.user.dto.response.UserPreviewDto;
 import space.banterbox.feature.user.model.User;
 import space.banterbox.feature.user.model.UsersFollower;
 import space.banterbox.feature.user.model.UsersFollowerId;
